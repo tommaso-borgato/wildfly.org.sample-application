a48c523c9412e6abbcca3dc2fb2563f085a658a7
Tommaso Borgato <tborgato@redhat.com>
2024-05-22 18:48:12 +0200

    Use Infinispan remote server

--- src/main/java/org/wildfly/examples/GettingStartedEndpoint.java
+++ src/main/java/org/wildfly/examples/GettingStartedEndpoint.java
@@ -1,6 +1,11 @@
 package org.wildfly.examples;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.HeuristicMixedException;
+import jakarta.transaction.HeuristicRollbackException;
+import jakarta.transaction.NotSupportedException;
+import jakarta.transaction.RollbackException;
+import jakarta.transaction.SystemException;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -8,18 +13,19 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.io.Serializable;
+
 @Path("/")
-public class GettingStartedEndpoint {
+public class GettingStartedEndpoint implements Serializable {
 
-    @Inject
-    private GettingStartedService service;
+	@Inject
+	private GettingStartedService service;
 
-    @GET
-    @Path("/{name}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response sayHello(final @PathParam("name") String name) {
-        String response = service.hello(name);
-
-        return Response.ok(response).build();
-    }
+	@GET
+	@Path("/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello(final @PathParam("name") String name) throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+		String response = service.hello(name);
+		return Response.ok(response).build();
+	}
 }
--- src/main/java/org/wildfly/examples/GettingStartedService.java
+++ src/main/java/org/wildfly/examples/GettingStartedService.java
@@ -1,11 +1,20 @@
 package org.wildfly.examples;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpServletRequest;
 
-@ApplicationScoped
-public class GettingStartedService {
+import java.io.Serializable;
+
+@SessionScoped
+public class GettingStartedService implements Serializable {
+
+    @Inject
+    private HttpServletRequest httpRequest;
 
     public String hello(String name) {
+        System.out.println("Previous name: " + httpRequest.getSession().getAttribute("NAME_PARAMETER"));
+        httpRequest.getSession().setAttribute("NAME_PARAMETER", name);
         return String.format("Hello '%s'.", name);
     }
 }
